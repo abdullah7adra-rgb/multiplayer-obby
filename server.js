d71878c966456a6b4694b978da9118cdf9d2e128
abdullah7adra-rgb <abdullah7adra@gmail.com>
2026-02-09 02:39:37 +0400

    Update server.js

--- server.js
+++ server.js
@@ -4,26 +4,22 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
-// This tells the server to show the files in the 'public' folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 
 let players = {};
 
 io.on('connection', (socket) => {
-    // When a new player joins
-    players[socket.id] = { x: 0, y: 0, z: 0, id: socket.id };
-    
-    // Broadcast movement data to everyone
+    players[socket.id] = { x: 0, y: 0, z: 0 };
+
     socket.on('move', (data) => {
         if (players[socket.id]) {
             players[socket.id].x = data.x;
             players[socket.id].y = data.y;
             players[socket.id].z = data.z;
-            socket.broadcast.emit('playerMoved', players[socket.id]);
+            socket.broadcast.emit('playerMoved', { id: socket.id, ...players[socket.id] });
         }
     });
 
-    // Clean up when someone leaves
     socket.on('disconnect', () => {
         delete players[socket.id];
         io.emit('playerDisconnected', socket.id);
@@ -32,5 +28,7 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-    console.log('Server is live on port ' + PORT);
+    console.log(`Server running on port ${PORT}`);
 });
+
+
