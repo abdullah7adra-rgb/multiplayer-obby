dcad7fc1125005507ff6ade1b33b0c0a05b0f707
abdullah7adra-rgb <abdullah7adra@gmail.com>
2026-02-09 06:31:16 +0400

    Update server.js

--- server.js
+++ server.js
@@ -3,37 +3,27 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
-
 app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'index.html')); });
 app.use(express.static(__dirname)); 
-
 let players = {};
-
 io.on('connection', (socket) => {
     socket.on('checkUsername', (name) => {
         const isTaken = Object.values(players).some(p => p.name === name);
         socket.emit('usernameResult', { success: !isTaken, name: name });
     });
-
     socket.on('join', (data) => {
-        players[socket.id] = { name: data.name, x: 0, y: 0, z: 0 };
+        players[socket.id] = { name: data.name };
         io.emit('currentPlayers', players);
     });
-
     socket.on('chatMessage', (msg) => {
-        if (players[socket.id]) {
-            io.emit('newMessage', { name: players[socket.id].name, text: msg });
-        }
+        if (players[socket.id]) io.emit('newMessage', { name: players[socket.id].name, text: msg });
     });
-
-    socket.on('voiceSignal', (data) => {
-        socket.broadcast.emit('voiceSignal', { from: socket.id, signal: data });
+    socket.on('voiceData', (data) => {
+        socket.broadcast.emit('voiceStream', { id: socket.id, buffer: data });
     });
-
     socket.on('disconnect', () => {
         delete players[socket.id];
         io.emit('playerDisconnected', socket.id);
     });
 });
-
 http.listen(process.env.PORT || 3000);
